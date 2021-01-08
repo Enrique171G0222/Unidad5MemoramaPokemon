ae931a5eaaa15383e5649692cbc327021141db1f
Enrique171G0222 <71042579+Enrique171G0222@users.noreply.github.com>
2021-01-08 01:18:32 -0600

    Add files via upload

---
+++ MemoramaPokemonWpf/Models/Pokemon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MemoramaPokemonWpf.Models.Pokemones;
+using Type = MemoramaPokemonWpf.Models.Pokemones.Type;
+
+namespace MemoramaPokemonWpf.Models
+{
+    public class Pokemon
+    {
+        public string imagen { get; set; }
+    }
+}
